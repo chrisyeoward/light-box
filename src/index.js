0fcd6b3b32de2611514d2f9a427f2b98c8d13666
Chris Yeoward <cyeoward@thoughtworks.com>
2020-05-03 12:30:21 +0100

    removes vue, adds webpack, uses particles for points

---
+++ src/index.js
@@ -0,0 +1,220 @@
+import * as THREE from 'three';
+
+
+const SEPARATION = 100, AMOUNTX = 50, AMOUNTY = 50;
+
+let container;
+let camera, scene, renderer;
+
+let particles;
+
+let mouseX = 0, mouseY = 0;
+
+let windowHalfX = window.innerWidth / 2;
+let windowHalfY = window.innerHeight / 2;
+
+const audioCtx = new AudioContext();
+let analyser;
+
+let powerBuffer;
+
+init();
+// animate();
+
+navigator.mediaDevices.getUserMedia({audio: true, video: false})
+    .then((stream) => {
+        const source = audioCtx.createMediaStreamSource(stream);
+        analyser = audioCtx.createAnalyser();
+
+        source.connect(analyser);
+
+        analyser.fftSize = 1024;
+
+        animate();
+    });
+
+// function buildDots({scene}) {
+//     let dots = [];
+//     const distance = 20;
+//     const dotSpacing = 0.3;
+//
+//     for(let i = -distance; i < distance; i += dotSpacing) {
+//         for (let j = -distance; j < distance; j+= dotSpacing) {
+//             geometry = new THREE.SphereGeometry(0.02, 0.2, 0.2);
+//             material = new THREE.MeshBasicMaterial();
+//             mesh = new THREE.Mesh(geometry, material);
+//             material.color = new THREE.Color(1,1,1);
+//             material.opacity = 0.0001;
+//             mesh.position.x =  i;
+//             mesh.position.z = j;
+//
+//             scene.add(mesh);
+//             dots.push(mesh);
+//         }
+//     }
+//
+//     return dots;
+// }
+
+function init() {
+
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
+
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera.position.z = 1000;
+
+    scene = new THREE.Scene();
+
+    //
+
+    let numParticles = AMOUNTX * AMOUNTY;
+
+    let positions = new Float32Array( numParticles * 3 );
+    let scales = new Float32Array( numParticles );
+
+    let i = 0, j = 0;
+
+    for ( let ix = 0; ix < AMOUNTX; ix ++ ) {
+
+        for ( let iy = 0; iy < AMOUNTY; iy ++ ) {
+
+            positions[ i ] = ix * SEPARATION - ( ( AMOUNTX * SEPARATION ) / 2 ); // x
+            positions[ i + 1 ] = 0; // y
+            positions[ i + 2 ] = iy * SEPARATION - ( ( AMOUNTY * SEPARATION ) / 2 ); // z
+
+            scales[ j ] = 1;
+
+            i += 3;
+            j ++;
+
+        }
+
+    }
+
+    let geometry = new THREE.BufferGeometry();
+    geometry.setAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
+    geometry.setAttribute( 'scale', new THREE.BufferAttribute( scales, 1 ) );
+
+    let material = new THREE.ShaderMaterial( {
+
+        uniforms: {
+            color: { value: new THREE.Color( 0xffffff ) },
+        },
+        vertexShader: document.getElementById( 'vertexshader' ).textContent,
+        fragmentShader: document.getElementById( 'fragmentshader' ).textContent
+
+    } );
+
+    //
+
+    particles = new THREE.Points( geometry, material );
+    scene.add( particles );
+
+    //
+
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
+
+    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+    document.addEventListener( 'touchstart', onDocumentTouchStart, false );
+    document.addEventListener( 'touchmove', onDocumentTouchMove, false );
+
+    //
+
+    window.addEventListener( 'resize', onWindowResize, false );
+}
+
+function averagePower(array) {
+    let length = 0.0;
+    return array.reduce((acc, sample) => {
+        length++;
+        return acc + Math.abs(sample);
+    }, 0) / length;
+}
+
+// let lastVal = 0;
+function animate() {
+    let bufferLength = analyser.fftSize;
+    let data = new Float32Array(bufferLength);
+
+    function draw() {
+        requestAnimationFrame( draw );
+
+        analyser.getFloatTimeDomainData(data);
+
+        let amp = averagePower(data);
+        render(amp);
+    }
+
+    draw();
+}
+
+function render(amp) {
+
+    camera.position.x += ( mouseX - camera.position.x ) * .05;
+    camera.position.y += ( - mouseY - camera.position.y ) * .05;
+    camera.lookAt( scene.position );
+
+    let positions = particles.geometry.attributes.position.array;
+    let scales = particles.geometry.attributes.scale.array;
+
+    let i = 0, j = 0;
+
+    for ( let ix = 0; ix < AMOUNTX; ix ++ ) {
+
+        for ( let iy = 0; iy < AMOUNTY; iy ++ ) {
+
+            // positions[ i + 1 ] = amp * 50 + ( amp * 0.5 ) * 50;
+
+            scales[ j ] = amp  * 100;
+
+            i += 3;
+            j ++;
+        }
+    }
+
+    // particles.geometry.attributes.position.needsUpdate = true;
+    particles.geometry.attributes.scale.needsUpdate = true;
+
+    renderer.render( scene, camera );
+}
+
+function onWindowResize() {
+    windowHalfX = window.innerWidth / 2;
+    windowHalfY = window.innerHeight / 2;
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+//
+
+function onDocumentMouseMove( event ) {
+    mouseX = event.clientX - windowHalfX;
+    mouseY = event.clientY - windowHalfY;
+}
+
+function onDocumentTouchStart( event ) {
+    if ( event.touches.length === 1 ) {
+
+        event.preventDefault();
+
+        mouseX = event.touches[ 0 ].pageX - windowHalfX;
+        mouseY = event.touches[ 0 ].pageY - windowHalfY;
+    }
+}
+
+function onDocumentTouchMove( event ) {
+    if ( event.touches.length === 1 ) {
+
+        event.preventDefault();
+
+        mouseX = event.touches[ 0 ].pageX - windowHalfX;
+        mouseY = event.touches[ 0 ].pageY - windowHalfY;
+    }
+}
